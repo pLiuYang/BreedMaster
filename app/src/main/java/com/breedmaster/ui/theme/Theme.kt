f7526ec716cb003ce64afda3fff0d5f18ebbd3bc
packerbc2 <a0077978@gmail.com>
2024-11-24 09:24:16 +0800

    Clear code analysis warnings

--- app/src/main/java/com/breedmaster/ui/theme/Theme.kt
+++ app/src/main/java/com/breedmaster/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.breedmaster.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
